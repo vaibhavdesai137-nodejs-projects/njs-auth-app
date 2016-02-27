279b2980121b4d846a27fdc00e96840b702af3fc
Vaibhav Desai <vaidesai@ebay.com>
2016-02-26 23:10:27 -0800

    final checkin

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 var configs = {
-    "dev": {
+    dev: {
         dbHost: "127.0.0.1",
         dbPort: 27017,
         dbName: "njs-auth-app-db",
         dbUser: "njs-auth-app-user",
         dbPassword: "njs-auth-app-user"
     },
-    "heroku": {
+    heroku: {
         dbHost: "ds017678.mlab.com",
         dbPort: "17678",
         dbName: "njs-auth-app-db",
@@ -16,8 +16,10 @@ var configs = {
 }
 
 // change this heroku deployment
-var currentEnv = "dev";
+var currentEnv = "heroku";
+myconfig = configs[currentEnv];
 
-module.exports.configs = function() {
-    return configs.currentEnv;
-}
+console.log('Current env: ' + currentEnv);
+console.log('Configs are: ' + myconfig);
+
+module.exports = myconfig;
